23ed783b343c459413f5e25019114bdca61c217d
LLanO <gosullano@gmail.com>
2020-04-16 04:58:13 -0400

    Agregando todo el proyecto con git add .

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Students;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Students::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Students::create($request->all());
+    }
+}
